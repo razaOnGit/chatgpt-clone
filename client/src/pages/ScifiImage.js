fb184c9be58edeb0c6b65eb033508b582a8f944c
MD RAZA KHAN <kmdraza47@gmail.com>
2024-11-03 17:47:59 +0530

    data

--- client/src/pages/ScifiImage.js
+++ client/src/pages/ScifiImage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+//import toast from "react-hot-toast";
 import axios from "axios";
 import {
   Box,
