8972be4eacb19ae4d62c92592e14a8c3158256e6
MD RAZA KHAN <kmdraza47@gmail.com>
2025-02-18 22:15:26 +0530

    d

--- server.js
+++ server.js
@@ -39,12 +39,9 @@ const geminiRoutes = require("./routes/geminiRoutes");
 const app = express();
 
 //middlewares
-app.use(cors({
-  origin: ['http://localhost:3000', 'your-frontend-domain.vercel.app'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(errorHandler);
 
