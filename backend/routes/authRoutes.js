06b087d6c79ed63de5f930e3dcf4b308071f9cf6
MD RAZA KHAN <kmdraza47@gmail.com>
2025-02-22 02:21:55 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,7 +13,10 @@ const router = express.Router();
 router.post("/register", registerContoller);
 
 //LOGIN
-router.post("/login", loginController);
+// router.post("/login", loginController);
+router.get("/login", (req, res) => {
+  res.send("GET request received at /login - please use POST for login functionality.");
+});
 
 //LOGOUT
 router.post("/logout", logoutController);
