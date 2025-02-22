b7115882c24125f6cd8faee534ea27b909c2bd70
MD RAZA KHAN <kmdraza47@gmail.com>
2025-02-22 11:08:34 +0530

    de

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "https://ai2backend.vercel.app"; // Fallback for local development
+const API_URL = import.meta.env.VITE_API_URL ; // Fallback for local development
 
 const api = axios.create({
   baseURL: API_URL,
