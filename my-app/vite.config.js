fbc8bab5c03136ee475cffe31169a1b73b82ead0
MD RAZA KHAN <kmdraza47@gmail.com>
2025-07-02 18:18:00 +0530

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
- base: process.env.NODE_ENV === 'production' ? './' : '/', // ✅ for relative paths on Vercel
+ base:'./',
   plugins: [react()],
   server: {
     port: 3000,
