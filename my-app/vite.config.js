c6dfdf15648659a1dbf1ecdd9fb0f5d99896d551
MD RAZA KHAN <kmdraza47@gmail.com>
2025-07-02 16:35:32 +0530

    idx

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-app/' : '/',
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
